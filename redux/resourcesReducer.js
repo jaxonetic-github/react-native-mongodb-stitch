349c8ae5cd6746d3908a809f8883b4afabe278d9
Aiti Maat El <zo@Ayiti-Ptah-Bey-2.local>
2019-04-23 14:24:33 -0700

    added resources and sidebar data to redux state

---
+++ redux/resourcesReducer.js
@@ -0,0 +1,14 @@
+//import {  } from '../../../../redux/types';
+
+/**       ::TODO::
+ * handle state for resource data(i.e.  youtube, web links)
+ */
+const resourcesReducer = (state={}, action) => {
+
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+export default resourcesReducer;
