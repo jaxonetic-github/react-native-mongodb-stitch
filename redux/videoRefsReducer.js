038c525e2575738bcc3df4b53033abf555bb0ef0
Aiti Maat El <zo@Ayiti-Ptah-Bey-2.local>
2019-04-29 23:39:51 -0700

    added video reducer

---
+++ redux/videoRefsReducer.js
@@ -0,0 +1,14 @@
+//import {  } from '../../../../redux/types';
+
+/**       
+ * Reducer for the videoReferences part of state
+ */
+const videoRefsReducer = (state={}, action) => {
+
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+export default videoRefsReducer;
