a3603dc7a4149b476794621a9c3885d1b1e21b6d
Aiti Maat El <zo@Ayiti-Ptah-Bey-2.local>
2019-06-18 08:44:51 -0700

    gh-pages deploy test 37

---
+++ docs/precache-manifest.81ad402545ec7e46770663c60ee3a64e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc8dcde5e291c54615e6f5a5a8b8ff3",
+    "url": "/react-native-mongodb-stitch/index.html"
+  },
+  {
+    "revision": "8a3fae5342e2340f0a14",
+    "url": "/react-native-mongodb-stitch/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "67e2848d9856b65df868",
+    "url": "/react-native-mongodb-stitch/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8a3fae5342e2340f0a14",
+    "url": "/react-native-mongodb-stitch/static/js/2.e7f7fe22.chunk.js"
+  },
+  {
+    "revision": "67e2848d9856b65df868",
+    "url": "/react-native-mongodb-stitch/static/js/main.b20fb97a.chunk.js"
+  },
+  {
+    "revision": "6420a3d0db9e92ca675b",
+    "url": "/react-native-mongodb-stitch/static/js/runtime~main.09a1a849.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a2eec654d8b3a245029420a8d6a5056.js"
+  "/react-native-mongodb-stitch/precache-manifest.81ad402545ec7e46770663c60ee3a64e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-native-mongodb-stitch/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
