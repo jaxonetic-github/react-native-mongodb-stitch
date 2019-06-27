d764a65e234fbc9dd0b5914865a746762b2d3f10
Aiti Maat El <zo@Ayiti-Ptah-Bey-2.local>
2019-06-27 12:43:41 -0700

    deploy to dist/ instead of docs/

---
+++ dist/precache-manifest.fd1e0b63f9a8c2551c3878efc765c7fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d8a5aa88d4a0d98a9cee9803470160",
+    "url": "/react-native-mongodb-stitch/index.html"
+  },
+  {
+    "revision": "429230a28bc072769c7b",
+    "url": "/react-native-mongodb-stitch/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "706f4ee11e8e9adfc371",
+    "url": "/react-native-mongodb-stitch/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "429230a28bc072769c7b",
+    "url": "/react-native-mongodb-stitch/static/js/2.f214272f.chunk.js"
+  },
+  {
+    "revision": "706f4ee11e8e9adfc371",
+    "url": "/react-native-mongodb-stitch/static/js/main.61a21875.chunk.js"
+  },
+  {
+    "revision": "6420a3d0db9e92ca675b",
+    "url": "/react-native-mongodb-stitch/static/js/runtime~main.09a1a849.js"
+  }
+]);
